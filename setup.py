0da0185e04d07eb07ae3bf0e319fe1c9d5e5c783
Aaron Fraint <aaronfraint@altaplanning.com>
2019-09-17 14:48:16 -0700

    setup file for this package

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""
+Overview of ``setup.py``
+------------------------
+This script is required for the ``pip install`` process to work.
+"""
+
+from setuptools import setup, find_packages
+
+long_description = """
+postGIS_tools is a pip-installable helper module 
+that supports spatial data science work in PostgreSQL/PostGIS
+"""
+
+with open('requirements.txt') as f:
+    requirements_lines = f.readlines()
+install_requires = [r.strip() for r in requirements_lines]
+
+pkgs = find_packages()
+
+setup(
+    name='alta_data_science',
+    version='1.0.0',
+    author='Aaron Fraint, AICP',
+    author_email='aaronfraint@altaplanning.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/aaronfraint/postGIS-tools.git',
+    packages=pkgs,
+    install_requires=install_requires,
+)
