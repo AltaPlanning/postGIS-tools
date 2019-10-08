d40fe016bc166c2889ad710e6cd7242f38487686
Aaron Fraint <aaronfraint@altaplanning.com>
2019-10-07 20:23:30 -0700

    dynamically read the user's OS into THIS_SYSTEM

--- postGIS_tools/assumptions.py
+++ postGIS_tools/assumptions.py
@@ -1,6 +1,7 @@
+import platform
 
 # This is a placeholder password for PostgreSQL.
 # The assumption is that users will pass their own password into the functions that they use
 PG_PASSWORD = "capacity-sasquatch-gnarly"
 
-THIS_SYSTEM = ""
+THIS_SYSTEM = platform.system()
