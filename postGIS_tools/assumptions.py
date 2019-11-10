69abb58225cb17d6edd9e183d79a8c089fd7eaaf
Aaron Fraint <aaronfraint@altaplanning.com>
2019-11-10 08:56:37 -0800

    add local config definition, ignore it from git, and use a stand-in -sample file for the public repo

--- postGIS_tools/assumptions.py
+++ postGIS_tools/assumptions.py
@@ -1,7 +1,26 @@
+import os
 import platform
+import configparser
+
+
+config = configparser.ConfigParser()
+
+if os.path.exists("../config.txt"):
+    config_path = "../config.txt"
+else:
+    config_path = "../config-sample.txt"
+
+config.read(config_path)
+
+configurations = {}
+for host in ["DEFAULT", "work_cpu", "digitalocean"]:
+    configurations[host] = {key: config[host][key] for key in config[host]}
+
+for section in configurations:
+    print(configurations[section])
 
 # This is a placeholder password for PostgreSQL.
 # The assumption is that users will pass their own password into the functions that they use
-PG_PASSWORD = "capacity-sasquatch-gnarly"
+PG_PASSWORD = configurations["DEFAULT"]["password"]
 
 THIS_SYSTEM = platform.system()
