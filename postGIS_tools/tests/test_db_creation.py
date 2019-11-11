6ce6b458b81d2833556963018cfe5b19c4c431e1
Aaron Fraint <aaronfraint@altaplanning.com>
2019-11-10 21:13:01 -0800

    add tests

---
+++ postGIS_tools/tests/test_db_creation.py
@@ -0,0 +1,18 @@
+from postGIS_tools.db.create import make_new_database
+
+from postGIS_tools.assumptions import CONFIG_FULL
+
+print(CONFIG_FULL)
+
+if __name__ == "__main__":
+
+    db_name = "test_db"
+
+    # Make a local database
+    local_config = CONFIG_FULL["localhost"]
+    make_new_database(db_name, debug=True, **local_config)
+
+    # Make a database on the D.O. cloud
+    do_config = CONFIG_FULL["digitalocean"]
+    print(CONFIG_FULL)
+    make_new_database(db_name, debug=True, **do_config)
