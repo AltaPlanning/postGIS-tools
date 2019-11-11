4ec0fc10f2751ac5a5bb72e0a4d27b2c7a6ead7f
Aaron Fraint <aaronfraint@altaplanning.com>
2019-11-11 12:42:39 -0800

    adjust test to match the new function

--- postGIS_tools/tests/test_db_creation.py
+++ postGIS_tools/tests/test_db_creation.py
@@ -1,18 +1,19 @@
 from postGIS_tools.db.create import make_new_database
 
-from postGIS_tools.configurations import CONFIG_FULL
+from postGIS_tools.configurations import get_postGIS_config
 
-print(CONFIG_FULL)
 
 if __name__ == "__main__":
 
-    db_name = "test_db"
+    CONFIG, CONFIG_FULL = get_postGIS_config()
+
+    DATABASE = "test_db"
 
     # Make a local database
     local_config = CONFIG_FULL["localhost"]
-    make_new_database(db_name, debug=True, **local_config)
+    make_new_database(DATABASE, debug=True, **local_config)
 
     # Make a database on the D.O. cloud
     do_config = CONFIG_FULL["digitalocean"]
     print(CONFIG_FULL)
-    make_new_database(db_name, debug=True, **do_config)
+    make_new_database(DATABASE, debug=True, **do_config)
