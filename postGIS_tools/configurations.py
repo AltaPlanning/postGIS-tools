1cf80cb4b6b88c80c749b251ead1016f898c809d
Aaron Fraint <aaronfraint@altaplanning.com>
2019-11-19 21:36:31 -0800

    refactor code and clean up printout

--- postGIS_tools/configurations.py
+++ postGIS_tools/configurations.py
@@ -4,6 +4,20 @@ Summary of ``configurations.py``
 
 TODO
 
+Examples
+--------
+
+    >>> from postGIS_tools.configurations import get_postGIS_config
+    >>> config, _ = get_postGIS_config()
+    - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ -
+    LOADING postGIS CONFIGURATIONS FROM /Users/aaron/Documents/pGIS-configurations/config.txt
+        * localhost *
+             {'host': 'localhost', 'username': 'postgres', 'password': 'your-password-here', 'port': '5432'}
+
+        * digitalocean *
+             {'username': 'your-username-here', 'host': 'your-host-here.db.ondigitalocean.com', 'password': 'your-password-here', 'port': '98765'}
+
+    - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ - - @ -
 """
 
 import os
@@ -18,8 +32,14 @@ PG_PASSWORD = "this-is-a-placeholder-password"
 
 THIS_SYSTEM = platform.system()
 
+SEPARATOR = "- @ - " * 14
+
+LOCAL_USER_CONFIG_FOLDER = "pGIS-configurations"
+
+
 def get_postGIS_config(
-        custom_config_folder: Union[bool, str] = None
+        custom_config_file: Union[bool, str] = None,
+        verbose: bool = True
 ) -> tuple:
     """
     Read a config.txt file and return a tuple wiht dictionaries for each postGIS configuration defined,
@@ -27,66 +47,68 @@ def get_postGIS_config(
     CONFIG_FULL also has keys for super_user and default_db
 
     :param custom_config_path:
+    :param verbose: boolean to print out configuration values
     :return:
     """
-    print(r"//\\//\\" * 10)
-    print("Loading PostGIS configurations:")
+    print(SEPARATOR)
 
-    # Use the specified custom folder if provided
-    if custom_config_folder:
-        local_config_folder = custom_config_folder
+    # Use the specified custom file if provided
+    if custom_config_file:
+        config_file = custom_config_file
 
-    # Otherwise, figure out the location dynamically by finding
-    # the user's OS-specific "Documents" folder
+    # Otherwise, figure out the filepath dynamically by finding
+    # the user's OS-specific "Documents" folder.
     else:
         # Get user and system
         THIS_USER = getpass.getuser()
 
         # Make filepath to config.txt
         if THIS_SYSTEM == "Darwin":
-            local_config_folder = f"/Users/{THIS_USER}/Documents/pGIS-configurations"
+            local_config_folder = f"/Users/{THIS_USER}/Documents/{LOCAL_USER_CONFIG_FOLDER}"
 
         elif THIS_SYSTEM == "Windows":
-            local_config_folder = rf"C:\Users\{THIS_USER}\My Documents\pGIS-configurations"
+            local_config_folder = rf"C:\Users\{THIS_USER}\My Documents\{LOCAL_USER_CONFIG_FOLDER}"
 
-    # Build the path to the "config.txt" file
-    local_config_file = os.path.join(local_config_folder, "config.txt")
+        # Build the path to the "config.txt" file
+        config_file = os.path.join(local_config_folder, "config.txt")
 
-    # Make it by copying the config-sample.txt if it does not yet exist
-    if not os.path.exists(local_config_file):
-        print(f"\t - Local config file does not yet exist. Copying to {local_config_folder}")
+        # Make it by copying the config-sample.txt if it does not yet exist
+        if not os.path.exists(config_file):
 
-        # Make the folder if it does not yet exist:
-        if not os.path.exists(local_config_folder):
-            os.mkdir(local_config_folder)
+            # Make the folder if it does not yet exist:
+            if not os.path.exists(local_config_folder):
+                os.mkdir(local_config_folder)
 
-        # Copy the sample file
-        shutil.copyfile("../config-sample.txt", local_config_file)
+            # Copy the sample file
+            shutil.copyfile("../config-sample.txt", config_file)
 
-    print(f"\t - {THIS_USER} on {THIS_SYSTEM}, using {local_config_file}")
+    print(f"LOADING postGIS CONFIGURATIONS FROM {config_file}")
 
     # Parse the config.txt
     config = configparser.ConfigParser()
-    config.read(local_config_file)
+    config.read(config_file)
 
-    # Make a CONFIG_FULL dict with everything and CONFIG without the keys_to_skip
-    SU_CONFIG = {}
-    keys_to_skip = ["default_db", "super_user", "super_user_pw"]
+    # Make a SUPERUSER_CONFIG dict with super user info and CONFIG without the keys_to_skip
     CONFIG = {}
+    SUPERUSER_CONFIG = {}
+    keys_to_skip = ["default_db", "super_user", "super_user_pw"]
     for host in config.sections():
-        SU_CONFIG[host] = {key: config[host][key] for key in config[host]}
         CONFIG[host] = {key: config[host][key] for key in config[host] if key not in keys_to_skip}
 
-        SU_CONFIG[host]["password"] = SU_CONFIG[host].pop("super_user_pw")
-        SU_CONFIG[host]["username"] = SU_CONFIG[host].pop("super_user")
+        SUPERUSER_CONFIG[host] = {key: config[host][key] for key in config[host]}
+
+        SUPERUSER_CONFIG[host]["password"] = SUPERUSER_CONFIG[host].pop("super_user_pw")
+        SUPERUSER_CONFIG[host]["username"] = SUPERUSER_CONFIG[host].pop("super_user")
 
     # Print out options defined in configuration file
-    print("\t - Available configurations:")
-    for host in CONFIG:
-        print(f"\t\t * {host} - {CONFIG[host]}")
-    print(r"\\//\\//" * 10, "\n")
+    if verbose:
+        for host in CONFIG:
+            print(f"\t* {host} *")
+            print(f"\t\t {CONFIG[host]} \n")
 
-    return (CONFIG, SU_CONFIG)
+    print(SEPARATOR, "\n")
+
+    return CONFIG, SUPERUSER_CONFIG
 
 if __name__ == "__main__":
     get_postGIS_config()
