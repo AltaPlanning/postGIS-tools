fb8660f5a90a1dc3af12d0392e1125e481c7d9ab
aaronfraint <38364429+aaronfraint@users.noreply.github.com>
2019-12-10 13:29:45 -0800

    Update test_db_creation.py

--- postGIS_tools/tests/test_db_creation.py
+++ postGIS_tools/tests/test_db_creation.py
@@ -1,21 +1,26 @@
 import postGIS_tools as pGIS
 
-from postGIS_tools.configurations import get_postGIS_config
+from postGIS_tools.configurations import get_postGIS_config, make_uri
+
+
+def test_database_creation(hostname):
+    DATABASE = "test_db"
+
+    print(f"<><> Testing the creation of {DATABASE} on {hostname}")
+    user_config, super_user_config = get_postGIS_config(verbose=True)
+
+    uri = make_uri(DATABASE, **user_config[hostname])
+    super_uri = make_uri(**super_user_config[hostname])
+
+    # Make a new database
+    pGIS.make_new_database(uri_defaultdb=super_uri, uri_newdb=uri, debug=False)
+
+    # Confirm it exists
+    if not pGIS.database_exists(DATABASE, uri=uri, default_db=super_user_config[hostname]["database"], debug=False):
+        print(f"<><> -> ERROR CREATING {DATABASE} AT {uri}")
 
 
 if __name__ == "__main__":
+    for config_host in ["localhost", "digitalocean_projects"]:
+        test_database_creation(config_host)
 
-    user_config, super_user_config = get_postGIS_config()
-    local_config = super_user_config["localhost"]
-    do_config = super_user_config["do_projects"]
-
-    DATABASE = "test_db"
-
-    for config in [local_config, do_config]:
-
-        # Make a new database
-        pGIS.make_new_database(DATABASE, **config)
-
-        # Confirm it exists
-        if not pGIS.database_exists(DATABASE, **config):
-            print(f"ERROR CREATING {DATABASE} AT {config}")
